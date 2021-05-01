4bee5e25bd7e1e92e52bc0a12cc159126987b66b
Devilly <de.slotenzwemmer@gmail.com>
2021-05-01 06:52:25 +0000

    Deploying to gh-pages from @ Devilly/DevBlog@4d7d5aa41337aec50ceff4fddef58c9672f1d05e 🚀

--- posts/___test/js/game.js
+++ posts/___test/js/game.js
@@ -3,9 +3,15 @@ export default class Game {
     #scale
     #lastPointerEvent
 
+    #gameWidth
+    #gameHeight
+
     constructor(canvas) {
         this.#canvas = canvas
         this.#setupCanvas(this.#canvas)
+
+        this.#gameWidth = 600
+        this.#gameHeight = this.#gameWidth / 2
     }
 
     #setupCanvas(canvas) {
@@ -19,36 +25,32 @@ export default class Game {
             canvas.setPointerCapture(event.pointerId)
 
             this.#lastPointerEvent = event
-        });
+        })
         canvas.addEventListener('pointermove', event => {
             this.#lastPointerEvent = event
-        });
+        })
         canvas.addEventListener('pointerup', event => {
             canvas.releasePointerCapture(event.pointerId)
 
             this.#lastPointerEvent = event
-        });
+        })
 
         // Adapt canvas width and height to how it is resized (by the CSS width rule).
         const resizeObserver = new ResizeObserver(entries => {
-            canvas.width = entries[0].contentBoxSize[0].inlineSize;
-            canvas.height = canvas.width / 2;
+            canvas.width = entries[0].contentBoxSize[0].inlineSize
+            canvas.height = canvas.width / 2
 
-            this.#scale = 600 / canvas.width;
-        });
-        resizeObserver.observe(canvas);
+            this.#scale = 600 / canvas.width
+        })
+        resizeObserver.observe(canvas)
     }
 
     start() {
         const canvas = this.#canvas
         const context = canvas.getContext('2d')
 
-        function formatPositionNumber(number) {
-            return number.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 })
-        }
-
         let main
-        ; (main = (lastTime, time) => {
+        ; (main = async (lastTime, time) => {
             window.requestAnimationFrame(main.bind(this, time))
 
             context.clearRect(0, 0, canvas.width, canvas.height)
@@ -59,32 +61,9 @@ export default class Game {
             // Time can be used to calculate timing based actions, e.g. animations and game movements.
             if (!lastTime) return
 
-            if (!lastPointerEvent) return
-
-            // Make sure the x and y positions are on the canvas, enabling a pointer to start on the canvas and continue outside of it.
-            const positionX = Math.min(canvas.width, Math.max(0, lastPointerEvent.clientX - canvas.getBoundingClientRect().left))
-            const positionY = Math.min(canvas.height, Math.max(0, lastPointerEvent.clientY - canvas.getBoundingClientRect().top))
-
-            const scaledPositionX = positionX * scale;
-            const scaledPositionY = positionY * scale;
-
-            const coordinatesText = `(${formatPositionNumber(scaledPositionX)}, ${formatPositionNumber(scaledPositionY)})`
-
-            const textMetrics = context.measureText(coordinatesText)
-            // https://developer.mozilla.org/en-US/docs/Web/API/TextMetrics#measuring_text_width
-            const textWidth = Math.abs(textMetrics.actualBoundingBoxLeft) + Math.abs(textMetrics.actualBoundingBoxRight)
-
             context.save()
 
-            if (positionX + textWidth > canvas.width) {
-                context.textAlign = 'end'
-            }
-
-            if (positionY < textMetrics.actualBoundingBoxAscent) {
-                context.textBaseline = 'top'
-            }
-
-            context.fillText(coordinatesText, positionX, positionY)
+            
 
             context.restore()
         })()
---
+++ posts/my-krita-workflow/text.md
@@ -0,0 +1,18 @@
+I use Krita mostly for helping me out with coding, creating custom images to work with. For this purpose I want to be able to quickly draw some simple shapes and be able to look at those with different zoom levels at the same time.
+
+## Setup Krita
+
+### Fix pressure sensitivity (if necessary)
+
+Go to `Settings` > `Configure Krita` > `Tablet settings` and set `Tablet input API` to `Windows Ink`.
+
+### Enable showing multiple views side by side
+
+Go to `Settings` > `Configure Krita` > `General` > `Window` and set `Multiple Document Mode` to `Subwindows`.
+
+## Use Krita
+
+### Show multiple views side by side
+
+1. Go to `Window` > `New View` and select your view for which you want another one.
+2. Go to `Window` and select `Tile` to show the views side by side.
